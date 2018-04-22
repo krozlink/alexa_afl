5b13707cf24acbd55ad1af7e9b6f44cf3dc41d53
Daniel Taylor <dmtaylor17@gmail.com>
2018-04-22 14:50:26 +1000

    Added sample alexa code

--- functions/win_next/main.go
+++ functions/win_next/main.go
@@ -1,20 +1,85 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log"
+
 	"github.com/aws/aws-lambda-go/lambda"
-	//alexa "github.com/ericdaugherty/alexa-skills-kit-golang"
+	alexa "github.com/ericdaugherty/alexa-skills-kit-golang"
 )
 
-type Response struct {
-	Message string `json:"message"`
+var a = &alexa.Alexa{ApplicationID: "amzn1.ask.skill.f6128da6-7813-4025-a762-b78be8fc6863", RequestHandler: &HelloWorld{}, IgnoreApplicationID: true, IgnoreTimestamp: true}
+
+const cardTitle = "HelloWorld"
+
+// HelloWorld handles reqeusts from the HelloWorld skill.
+type HelloWorld struct{}
+
+// Handle processes calls from Lambda
+func Handle(ctx context.Context, requestEnv *alexa.RequestEnvelope) (interface{}, error) {
+	return a.ProcessRequest(ctx, requestEnv)
 }
 
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
+// OnSessionStarted called when a new session is created.
+func (h *HelloWorld) OnSessionStarted(ctx context.Context, request *alexa.Request, session *alexa.Session, ctxPtr *alexa.Context, response *alexa.Response) error {
+
+	log.Printf("OnSessionStarted requestId=%s, sessionId=%s", request.RequestID, session.SessionID)
+
+	return nil
+}
+
+// OnLaunch called with a reqeust is received of type LaunchRequest
+func (h *HelloWorld) OnLaunch(ctx context.Context, request *alexa.Request, session *alexa.Session, ctxPtr *alexa.Context, response *alexa.Response) error {
+	speechText := "Welcome to the Alexa Skills Kit, you can say hello"
+
+	log.Printf("OnLaunch requestId=%s, sessionId=%s", request.RequestID, session.SessionID)
+
+	response.SetSimpleCard(cardTitle, speechText)
+	response.SetOutputText(speechText)
+	response.SetRepromptText(speechText)
+
+	response.ShouldSessionEnd = true
+
+	return nil
+}
+
+// OnIntent called with a reqeust is received of type IntentRequest
+func (h *HelloWorld) OnIntent(ctx context.Context, request *alexa.Request, session *alexa.Session, ctxPtr *alexa.Context, response *alexa.Response) error {
+
+	log.Printf("OnIntent requestId=%s, sessionId=%s, intent=%s", request.RequestID, session.SessionID, request.Intent.Name)
+
+	switch request.Intent.Name {
+	case "HelloWorldIntent":
+		log.Println("HelloWorldIntent triggered")
+		speechText := "Hello World"
+
+		response.SetSimpleCard(cardTitle, speechText)
+		response.SetOutputText(speechText)
+
+		log.Printf("Set Output speech, value now: %s", response.OutputSpeech.Text)
+	case "AMAZON.HelpIntent":
+		log.Println("AMAZON.HelpIntent triggered")
+		speechText := "You can say hello to me!"
+
+		response.SetSimpleCard("HelloWorld", speechText)
+		response.SetOutputText(speechText)
+		response.SetRepromptText(speechText)
+	default:
+		return errors.New("Invalid Intent")
+	}
+
+	return nil
+}
+
+// OnSessionEnded called with a reqeust is received of type SessionEndedRequest
+func (h *HelloWorld) OnSessionEnded(ctx context.Context, request *alexa.Request, session *alexa.Session, ctxPtr *alexa.Context, response *alexa.Response) error {
+
+	log.Printf("OnSessionEnded requestId=%s, sessionId=%s", request.RequestID, session.SessionID)
+
+	return nil
 }
 
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(Handle)
 }
